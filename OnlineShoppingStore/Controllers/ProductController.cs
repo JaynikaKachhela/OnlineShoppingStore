582cddfe6fa6a972d362ebfbe02f5151bcacff8c
JaynikaKachhela <jaynika@promactinfo.com>
2016-07-06 16:58:40 +0530

    Add project files.

---
+++ OnlineShoppingStore/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShoppingStore.Models;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ViewResult List()
+        {
+            Product product1 = new Product();
+            product1.Name = "T-shirt";
+            product1.Price = 300;
+            product1.Description = "colored with blue, orange, red, yellow...";
+                       
+            return View(product1);
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
